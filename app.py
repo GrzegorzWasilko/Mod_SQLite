59b97062cdd3085933c7d1aa9645850d5d4edf29
Grzegorz Wasilko <wasilko1@o2.pl>
2021-10-18 13:02:22 +0200

    Lets prey that its works

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def todos_list():
     return render_template("todos.html", form=form, todos=todos, error=error)
 
 
-@app.route('/todos/<int:id>/', methods=['GET','POST'])
+@app.route('/todos', methods=['GET','POST'])
 def update(id):
     if request.method=='GET':
         conn = sqlite3.connect('todos.db')
@@ -57,6 +57,35 @@ def update(id):
         conn.commit()
         conn.close()
     return render_template("todo.html", id=id)
+
+
+@app.route('/todos/<int:id>/', methods=['GET','POST'])
+def change(id):
+    form = TodoForm()
+    if request.method=='GET':
+        conn = sqlite3.connect('todos.db')
+        cursor=conn.cursor()
+        todo1=cursor.execute("SELECT * FROM todos WHERE id=?", (id,))
+        todo1= cursor.fetchall()
+        conn.commit()
+        cursor.close()
+        return (render_template("todo_id.html", todo1=todo1,form=form))
+
+
+    if request.method =='POST':
+        conn = sqlite3.connect('todos.db')
+        todo= request.form
+        id= todo['id']
+        title=todo['title']
+        description= todo['description']
+        done= todo['done']
+        cursor = conn.cursor()
+        cursor.execute("UPDATE todo SET id=?, title=?, description=?, done=? WHERE todo_id=?",(id,title,description,done))
+        conn.commit()
+        cursor.close()
+        conn.commit()
+        conn.close()
+    return render_template("todo.html", id=id,form=form)
 
 if __name__ == "__main__":
     app.run(debug=True) 
